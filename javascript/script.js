c348b9738e4e80aef9d78310d7e1410940bfc847
kucingliaw <wawal.wiguna@gmail.com>
2022-02-10 09:29:13 +0700

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+document.writeln("<p>Hello World</p>")
+document.writeln("<p>My name is Killua</p>")
+// ini komentar
+/*  ini
+    komentar
+    lebih dari satu baris */ 
+document.writeln("<p>Nice to meet you!</p>")
+document.writeln(100)
